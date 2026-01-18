41eaa31ce3ca79a219a22c94f5b59fe4cd29ad08
Jagriti Sharma <nidhisharma7241@gmail.com>
2026-01-19 01:51:12 +0530

    Update auth.controllers.js

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -36,7 +36,7 @@ export const signup = async(req, res) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "None",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
      })
 
@@ -82,7 +82,7 @@ export const login = async(req,res) =>{
     res.cookie("token", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "None",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
      })
 
